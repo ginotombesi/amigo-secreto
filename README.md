98b1263cbba8199ca2910795c28ab417407ed471
Gino Paolo Tombesi <95345@sistemas.frc.utn.edu.ar>
2025-01-27 14:37:42 -0300

    Agrego funcion agregarAmigos()

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# amigo-secreto
+# Smigo secreto
 Aplicación para gestionar un sorteo de "amigo secreto". Los usuarios agregan nombres mediante un campo de texto y un botón "Adicionar". Los nombres se muestran en una lista, y al finalizar, un botón "Sortear Amigo" elige aleatoriamente un nombre, mostrando el resultado en pantalla.
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo === "") {
+        alert("Por favor, ingresa un nombre de amigo.");
+        return;
+    }
+    amigos.push(amigo);
+    document.getElementById("amigo").value = "";
+}
