9eccdc8b7410a31432093a3c100cb752900ec5ec
Gino Paolo Tombesi <95345@sistemas.frc.utn.edu.ar>
2025-01-27 14:50:08 -0300

    agrego funcionalidad sortearAmigo()

--- app.js
+++ app.js
@@ -21,3 +21,12 @@ function actualizarLista() {
         lista.appendChild(item);
     });
 }
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.getElementById("resultado").textContent = "El amigo sorteado es: " + amigoSorteado;
+}
